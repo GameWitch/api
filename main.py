2663a8667a3548ded8a867f2fcd3a55af37e4d81
Dylan <Dmactavishf@gmail.com>
2022-08-31 13:12:12 -0600

    updated url examples in index.html

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from os import environ
 
 app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL', 'sqlite:///Data/addresses.db')
+# for local
+# app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL', 'sqlite:///Data/addresses.db')
+# for deployed
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
